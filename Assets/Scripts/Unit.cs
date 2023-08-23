55c34d21872a622dfaf3b4046eef0ffebde1e7da
Potat07 <130532986+Potat07@users.noreply.github.com>
2023-08-23 15:03:45 +0930

    Add files via upload

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+	public string unitName;
+	public int unitLevel;
+	public float damage;
+	public float defense;
+	public float maxHP;
+	public float currentHP;
+
+
+    public bool TakeDamage(float dmg)
+	{
+		currentHP -= dmg;
+
+		if (currentHP <= 0)
+			return true;
+		else
+			return false;
+	}
+
+
+}
