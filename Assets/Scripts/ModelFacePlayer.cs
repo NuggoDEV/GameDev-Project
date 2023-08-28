9d48762d79a0d17791805c61640d54745d031745
Sneakiestmcbeakiest <taranandthekittens@outlook.com>
2023-08-28 09:52:35 +0930

    Made the Zombies chase AI, still need to add attacking

---
+++ Assets/Scripts/ModelFacePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelFacePlayer : MonoBehaviour
+{
+    public GameObject playerCore;
+    public GameObject self;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCore = GameObject.FindGameObjectWithTag("PlayerCore");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(playerCore.transform);
+    }
+}
