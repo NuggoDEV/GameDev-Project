dcec5e4889f4e14b334a85cd5af959b908f6735e
Potat07 <130532986+Potat07@users.noreply.github.com>
2023-11-08 15:30:17 +1030

    Add files via upload

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -9,15 +9,21 @@ public class UserInterface : MonoBehaviour
     [SerializeField] Slider hpSlider;
     [SerializeField] TextMeshProUGUI waveText;
     [SerializeField] Unit playerUnit;
+    public TextMeshProUGUI SunText;
+    public TextMeshProUGUI CoinText;
+    GameManager gameManager;
 
     private void Start()
     {
         hpSlider.maxValue = playerUnit.maxHP;
-        GameObject.Find("GameManager").GetComponent<WaveManager>().startWave += WaveStart;
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.GetComponent<WaveManager>().startWave += WaveStart;
     }
     void Update()
     {
         hpSlider.value = playerUnit.currentHP;
+        SunText.text = gameManager.Sun.ToString();
+        CoinText.text = gameManager.Coins.ToString();
     }
 
     private void WaveStart(int wave, int count) => waveText.text = $"Wave: {wave}";
